443ed095bb93d3ca4c07eeae98e82d90087dcebe
ManalLiaquat <m.manalliaquat@gmail.com>
2018-05-28 02:43:46 +0500

    I add a service worker file for making it a Progressive Web App

---
+++ sw.js
@@ -0,0 +1,47 @@
+const cacheName = 'RapydSell-App';
+const staticAssets = [
+    './',
+    './css/style.css',
+    './index.html',
+    './rapydsell.html',
+    './adpost.html',
+    './js/app.js',
+    './media/favicon.ico',
+    './media/slider1.jpg',
+    './media/slider2.jpg',
+    './media/animation-intro.mp4'
+]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(staticAssets);
+        })
+    );
+})
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(req))
+    } else {
+        event.respondWith(networkFirst(req))
+    }
+})
+
+async function cacheFirst(req) {
+    const cacheResponse = await caches.match(req);
+    return cacheResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open(cacheName);
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone())
+        return res
+    } catch (error) {
+        return await cache.match(req)
+    }
+}
